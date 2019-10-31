63557162211e24b69fdbd207a0474912d737a271
Vladislav Lapidus <it.lapidus@gmail.com>
2019-10-31 21:53:56 +0200

    Initial Start

---
+++ MVC_CRUD/Migrations/20191031185437_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MVC_CRUD.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    nvarchar250 = table.Column<string>(name: "nvarchar(250)", nullable: false),
+                    nvarchar10 = table.Column<string>(name: "nvarchar(10)", nullable: true),
+                    nvarchar100 = table.Column<string>(name: "nvarchar(100)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmployeeId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
---
+++ MVC_CRUD/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_CRUD.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        [Column(TypeName = "nvarchar(250)")]
+        [Required]
+        public string FullName { get; set; }
+        [Column(TypeName = "nvarchar(10)")]
+        public string EmpCode { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string Position { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string OfficeLocation { get; set; }
+    }
+}
